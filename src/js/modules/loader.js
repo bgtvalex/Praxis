5b8720a1c5f97b8abfc39cf72abf0817237f7ed1
bgtvalex <bgtvalex@ya.ru>
2023-12-13 17:20:00 +0300

    update

--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -2,8 +2,11 @@ function loader() {
 	const loader = document.querySelector('#loader')
 
 	window.addEventListener('load', () => {
+
+		document.body.classList.add('no-scroll');
 		setTimeout(() => {
 			loader.classList.add('loader-wrapper--hidden')
+			document.body.classList.remove('no-scroll');
 		}, 1500)
 	})
 }
